f1ec08d4bb6916bcb6b3c11af7f74d4c3bdd32f8
Lauri Junkkari <latenssi@gmail.com>
2016-01-10 23:54:33 +0200

    Change order of authentication classes in DRF.

--- postal/settings.py
+++ postal/settings.py
@@ -141,9 +141,9 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
